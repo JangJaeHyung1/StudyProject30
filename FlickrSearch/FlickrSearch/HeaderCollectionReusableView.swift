a5d5dde4907df54b01e68cf14fe20ede2e9e3a64
JangJaeHyung1 <ggassoon@naver.com>
2021-09-24 16:48:35 +0900

    headerView 추가

---
+++ FlickrSearch/FlickrSearch/HeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCollectionReusableView.swift
+//  FlickrSearch
+//
+//  Created by jh on 2021/09/24.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
--- FlickrSearch/FlickrSearch/ViewController.swift
+++ FlickrSearch/FlickrSearch/ViewController.swift
@@ -12,9 +12,35 @@ class ViewController: UIViewController {
     private var searches: [FlickrSearchResults] = []
     private let flickr = Flickr()
     
+    // MARK: - drag n drop
+
+    var largePhotoIndexPath: IndexPath? {
+      didSet {
+        var indexPaths: [IndexPath] = []
+        if let largePhotoIndexPath = largePhotoIndexPath {
+          indexPaths.append(largePhotoIndexPath)
+        }
+
+        if let oldValue = oldValue {
+          indexPaths.append(oldValue)
+        }
+
+        imageCollectionView.performBatchUpdates({
+          self.imageCollectionView.reloadItems(at: indexPaths)
+        }, completion: { _ in
+          if let largePhotoIndexPath = self.largePhotoIndexPath {
+            self.imageCollectionView.scrollToItem(
+              at: largePhotoIndexPath,
+              at: .centeredVertically,
+              animated: true)
+          }
+        })
+      }
+    }
+
     
     @IBAction func selectBtn(_ sender: UIBarButtonItem) {
-        print("btn pressed")
+//        print("btn pressed")
         var selectedLabel = UIBarButtonItem(title: "0 photos selected", style: .plain, target: .none, action: nil)
         
         selectedLabel.tintColor = .systemGreen
@@ -29,6 +55,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         collectionCellUI()
     }
+    
+    
 }
 
 
@@ -53,6 +81,30 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UIScrollViewDelega
         return cell
     }
     
+    // MARK: - HeaderCollectionReusableView
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width, height: 90)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+        
+        case UICollectionView.elementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderCollectionReusableView", for: indexPath)
+            
+            guard let typedHeaderView = headerView as? HeaderCollectionReusableView else { return headerView }
+            
+            let searchTerm = searches[indexPath.section].searchTerm
+            typedHeaderView.titleLabel.text = searchTerm
+            
+            return typedHeaderView
+            
+        default:
+            assert(false, "Invalid element type")
+        }
+    }
 
     // MARK: - UICollectionViewDelegateFlowLayout
     
@@ -68,6 +120,19 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UIScrollViewDelega
         
         self.imageCollectionView.collectionViewLayout = flowLayout
     }
+    
+    
+    // MARK: - drag n drop IndexPath
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        if largePhotoIndexPath == indexPath {
+            largePhotoIndexPath = nil
+        } else {
+            largePhotoIndexPath = indexPath
+        }
+        return false
+    }
+    
+
     
 }
 
